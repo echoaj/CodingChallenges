c9209c7101aff4f0e64c3eb09cb14abe238b20e4
echoaj <aljoslin13@yahoo.com>
2020-10-11 15:57:59 -0700

    3Sum Implementation Needs work

---
+++ LeetCode/3Sum.py
@@ -0,0 +1,28 @@
+# LeetCode
+# Medium
+# O(n^2) solution
+
+# Needs Work
+def tsum(array):
+    array.sort()
+    i = 0
+    j = len(array)-1
+    results =[]
+
+    while i < j:
+
+        for k in range(i+1, j):
+            if array[i] + array[j] + array[k] == 0:
+                nums = [array[i], array[j], array[k]]
+                if nums not in results:
+                    results.append(nums)
+
+        if 0 - array[i] > array[j] - 0:
+            i += 1
+        else:
+            j -= 1
+
+    return results
+
+
+print(tsum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
