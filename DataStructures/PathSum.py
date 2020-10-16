21141ca59925635b0db0eef6b6c9370c07929158
echoaj <aljoslin13@yahoo.com>
2020-10-16 16:17:33 -0700

    Symmetric Tree implementation

--- DataStructures/PathSum.py
+++ DataStructures/PathSum.py
@@ -1,11 +1,9 @@
-# LeetCode
-# 113. Path Sum II
-# Medium
 
 """
-Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
 
 Example:
+
 Given the below binary tree and sum = 22,
 
       5
@@ -13,15 +11,9 @@ Given the below binary tree and sum = 22,
     4   8
    /   / \
   11  13  4
- /  \    / \
-7    2  5   1
-
-Return:
-
-[
-   [5,4,11,2],
-   [5,8,4,5]
-]
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 """
 
 
@@ -32,19 +24,18 @@ class Node:
         self.right = right
 
 
-array = []
-def pathSum(root, sum, path):
+
+def hasPathSum(root, sum):
     if root is None:
-        return
+        return False
 
-    path.append(root.val)
     if root.val == sum and root.left is None and root.right is None:    # reached leaf
-        array.append(path)
-        return
+        print(root.val)
+        return True
 
-    pathSum(root.left, sum-root.val, path.copy())
-    pathSum(root.right, sum-root.val, path.copy())
-
+    found1 = hasPathSum(root.left, sum-root.val)
+    found2 = hasPathSum(root.right, sum-root.val)
+    return found1 or found2
 
 
 tree = Node(5)
@@ -55,8 +46,7 @@ tree.left.left.right = Node(2)
 tree.right = Node(8)
 tree.right.left = Node(13)
 tree.right.right = Node(4)
-tree.right.right.left = Node(5)
 tree.right.right.right = Node(1)
 
-pathSum(tree, 22, [])
-print(array)
+print(hasPathSum(tree, 22))
+
---
+++ DataStructures/symmetric tree.py
@@ -0,0 +1,46 @@
+
+
+'''
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+'''
+
+
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isSymmetric(root):
+    if root is None:
+        return True
+    return sym(root.left, root.right)
+
+
+def sym(left_node, right_node):
+    if left_node is None is right_node:
+        return True
+    if left_node is None or right_node is None or left_node.val != right_node.val:
+        return False
+
+    return sym(left_node.left, right_node.right) and sym(left_node.right, right_node.left)
+
+
+tree = Node(1)
+tree.left = Node(2)
+tree.left.left = Node(3)
+tree.left.right = Node(4)
+tree.right = Node(2)
+tree.right.right = Node(3)
+tree.right.left = Node(4)
+
+
+print(isSymmetric(tree))
