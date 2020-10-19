ee7d9618e9a66a4355bea3f8dc2d4678e2f6dea7
echoaj <aljoslin13@yahoo.com>
2020-10-19 01:29:15 -0700

    Reverse linked List Implementation

---
+++ DataStructures/reverse_link_list.py
@@ -0,0 +1,39 @@
+# LeetCode
+# 206. Reverse Linked List
+# Easy
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseList(head):
+    prev = None
+    next_node = head.next
+
+    while next_node is not None:
+        head.next = prev
+        prev = head
+        head = next_node
+        next_node = next_node.next
+    head.next = prev
+
+    return head
+
+'''
+Better Solution
+def reverseList(self, head):
+    prev = None
+    while head:
+        curr = head
+        head = head.next
+        curr.next = prev
+        prev = curr
+    return prev
+'''
+
+
+l1 = ListNode(1, ListNode(2, ListNode(4, ListNode(5))))
+node = reverseList(l1)
+print(node.val)
