7f6cb5d0c5dbaf0eee2ee8594ecb3e29ca99c45e
echoaj <aljoslin13@yahoo.com>
2020-12-24 22:34:25 -0800

    mixed around the code

--- Random/max_steps_to_end.py
+++ Random/max_steps_to_end.py
@@ -8,12 +8,17 @@ you would move forward by.
 '''
 
 def max_steps(x):
-    if x >= len(array):
+    length = len(array)
+    if x >= length:
         return 0
     num = array[x]
-    result1 = max_steps(num+x+1)
     result2 = max_steps(num+x+2)
-    return max(result1, result2) + 1
+    result1 = max_steps(num+x+1)
+    total =  max(result1, result2) + 1
+    return total
 
-array = [0,9,0,0,3,0,5,1,2,0,1,0]
-print(max_steps(0))
+array = [0, 9, 0, 0, 3, 0, 5, 1, 2, 0, 1, 0]
+answer = max_steps(0)
+
+print(answer)
+
