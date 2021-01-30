b9e37febd11f59b1b5311f34bd8ce1d5d6b05594
echoaj <aljoslin13@yahoo.com>
2021-01-29 23:12:05 -0800

    set hope to hop

--- Random/max_steps_to_end.py
+++ Random/max_steps_to_end.py
@@ -2,7 +2,7 @@
 """
 array = [0,9,0,0,3,0,5,1,2,0,1,0]
 
-You can hope how many times you want and your hops
+You can hop how many times you want and your hops
 do not have a limit.  Count the maximum steps it would
 take to get to the end.  The numbers represent how much
 you would move forward by.
@@ -20,5 +20,3 @@ def max_steps(x):
 
 array = [0, 9, 0, 0, 3, 0, 5, 1, 2, 0, 1, 0]
 print(max_steps(0))
-
-
