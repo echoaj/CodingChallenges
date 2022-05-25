2e671eab0819535e5fab61a63457a1465fa10794
echoaj <aljoslin13@yahoo.com>
2022-05-25 00:16:24 -0700

    finished solution for 3sum

--- LeetCode/3Sum.py
+++ LeetCode/3Sum.py
@@ -1,27 +1,52 @@
-# LeetCode 15
+# LeetCode 15. 3Sum
 # Medium
-# O(n^2) solution
 
-# Needs Work
-def three_sum(array):
-    array.sort()
-    i = 0
-    j = len(array)-1
-    results =[]
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such
+# that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# Notice that the solution set must not contain duplicate triplets.
 
-    while i < j:
-        for k in range(i+1, j):
-            if array[i] + array[j] + array[k] == 0:
-                nums = [array[i], array[j], array[k]]
-                if nums not in results:
-                    results.append(nums)
+"""
+Example 1:
 
-        if 0 - array[i] > array[j] - 0:
-            i += 1
-        else:
-            j -= 1
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Example 2:
 
-    return results
+Input: nums = []
+Output: []
+Example 3:
+
+Input: nums = [0]
+Output: []
+"""
 
 
-print(three_sum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]))
+def three_sum(nums):
+    nums.sort()
+    target = 0
+    combos = []
+
+    for i, a in enumerate(nums):
+        if i > 0 and a == nums[i - 1]:
+            continue
+
+        l = i + 1
+        r = len(nums) - 1
+
+        while l < r:
+            total = a + nums[l] + nums[r]
+            if total > target:
+                r -= 1
+            elif total < target:
+                l += 1
+            else:
+                combos.append([a, nums[l], nums[r]])
+                l += 1
+                while nums[l] == nums[l - 1] and l < r:
+                    l += 1
+
+    return combos
+
+
+print(three_sum([-1, 0, 1, 2, -1, -4]))
+# Output: [[-1, -1, 2], [-1, 0, 1]]
