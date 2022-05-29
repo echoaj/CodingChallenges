e7e23d2f92a492d3985b45c63bd4d0e48d75afb3
echoaj <aljoslin13@yahoo.com>
2022-05-29 12:41:13 -0700

    group anagrams completed

--- LeetCode/group_anagrams.py
+++ LeetCode/group_anagrams.py
@@ -1,32 +1,30 @@
+# Leetcode Problem: https://leetcode.com/problems/group-anagrams/
+# Medium 49
+# Given an array of strings, group anagrams together.
 
-# Time Complexity
-# n * (1 + k + k + 1 + 1) + n + 1   -->  O(n * k)
+# Example:
+# Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+# Output:
+# [
+#   ["ate","eat","tea"],
+#   ["nat","tan"],
+#   ["bat"]
+# ]
+
+# Note:
+# All inputs will be in lowercase.
+# The order of your output does not matter.
 
 
-def group_anagrams(strings):
-    map = dict()
-    tracker = []
-
-    for string in strings:              #O(n)
-        numbers = ['0' for i in range(25)]  # O(1)
-        for i in string:                #O(k)
-            num = ord(i)-97
-            numbers[num] = '1'
-
-        result = ''.join(numbers)
-        if result not in map:
-            map[result] = [string]      #O(1)
+def group_anagrams(strs):
+    map_ = {}
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        if sorted_word in map_:
+            map_[sorted_word].append(word)
         else:
-            map[result].append(string)  #O(1)
-
-    #print(map)
-    for key in map:
-        tracker.append(map[key])
-
-    return tracker
+            map_[sorted_word] = [word]
+    return list(map_.values())
 
 
 print(group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
-
-# WRONG
-print(group_anagrams(["gggd", "dddg", "tan", "ate", "nat", "bat"]))
