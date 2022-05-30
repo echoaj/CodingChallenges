214f7ee528a2dfcad8274c9799ae73ed3fc76d67
Alex Joslin <aljoslin13@yahoo.com>
2022-05-30 01:21:02 -0700

    sort colors leetCode complete

---
+++ LeetCode/sort_colors.py
@@ -0,0 +1,54 @@
+# LeetCode 75. Sort Colors
+# https://leetcode.com/problems/sort-colors/
+# Medium
+
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that
+# objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+# You must solve this problem without using the library's sort function.
+
+"""
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+"""
+
+# O(n) time and O(1) space
+
+def sort_colors(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    # Initialize pointers
+    zeros = 0
+    ones = 0
+    twos = 0
+
+    for i in nums:
+        if i == 0:
+            zeros += 1
+        elif i == 1:
+            ones += 1
+        else:
+            twos += 1
+
+    for i in range(0, zeros):
+        nums[i] = 0
+    for i in range(zeros, zeros + ones):
+        nums[i] = 1
+    for i in range(zeros + ones, zeros + ones + twos):
+        nums[i] = 2
+
+
+if __name__ == "__main__":
+    nums = [2, 0, 2, 1, 1, 0]
+    sort_colors(nums)
+    print(nums)
